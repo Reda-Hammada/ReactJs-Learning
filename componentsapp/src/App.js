50a67669a50119728c5bbd8893bee096853a9882
reda hammada <hammada.reda.dev@gmail.com>
2022-05-08 04:12:54 +0100

    update components

---
+++ componentsapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Heading  from './components/component/Heading'; 
+function App() {
+  return (
+    <div className="App">
+      
+        <Heading />
+        
+    </div>
+  );
+}
+
+export default App;
