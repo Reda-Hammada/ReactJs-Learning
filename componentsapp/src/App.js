86d3c1b730dc7e8290a01c187fc5073d68eb7e58
reda hammada <hammada.reda.dev@gmail.com>
2022-05-08 18:43:39 +0100

    update components

--- componentsapp/src/App.js
+++ componentsapp/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import Heading  from './components/component/Heading'; 
+import Heading  from './components/Heading'; 
+import Greet from './components/Greet';
 function App() {
   return (
     <div className="App">
       
         <Heading />
-        
+        <Greet />
+
+
+
     </div>
   );
 }
--- componentsapp/src/components/Heading.jsx
+++ componentsapp/src/components/Heading.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../component/heading.css';
+import '../components/heading.css';
 
 function Heading(){
     return(
