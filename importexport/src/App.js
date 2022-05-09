931ee158fd81efb98052ddd3f840e1a202baf41f
reda hammada <hammada.reda.dev@gmail.com>
2022-05-09 18:43:19 +0100

    calc import and export example

--- importexport/src/App.js
+++ importexport/src/App.js
@@ -3,15 +3,12 @@ import * as calculator from './importExport/calc';
 function App() {
   return (
     <div className="App">
-      <div>
         <ul>
           <li>{calculator.add(1,6)}</li>
           <li>{calculator.divide(10,2)}</li>
           <li>{calculator.multiply(50,10)}</li>
           <li>{calculator.subtract(10, 3)}</li>
         </ul>
-      </div>
-      
     </div>
   );
 }
