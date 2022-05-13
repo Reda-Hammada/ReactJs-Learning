ae91f91759c21c4aa63143f6fbf9f18b40f5b19d
reda hammada <hammada.reda.dev@gmail.com>
2022-05-13 12:45:14 +0100

    update

--- componentsapp/src/App.js
+++ componentsapp/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import Heading  from './components/Heading'; 
 import Greet from './components/Greet';
+import Classcomponent from "../../componentsapp/src/components/Classcomponent.jsx";
+
 function App() {
   return (
     <div className="App">
+
       
         <Heading />
+        <Classcomponent />
         <Greet />
 
 
